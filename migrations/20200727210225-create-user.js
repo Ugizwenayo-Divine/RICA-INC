058c3456c2b6ccbeedbab74fbfcf8eaf9732850b
manzi-guev <manziguevara@gmail.com>
2020-07-27 21:40:54 +0200

    ch(backend): setting up sequelize

---
+++ migrations/20200727210225-create-user.js
@@ -0,0 +1,25 @@
+const up = (queryInterface, Sequelize) =>
+  queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING,
+    },
+    email: {
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  });
+const down = (queryInterface) => queryInterface.dropTable('Users');
+export { up, down };
