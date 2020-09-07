c55724931e7f36e39b32a9818b83e758f17a683d
Divine Ugizwenayo <ugizwenayodiny@gmail.com>
2020-09-06 22:52:36 -1000

    - payment

--- server/routes/api/payment.js
+++ server/routes/api/payment.js
@@ -9,11 +9,13 @@ const {
 const {
   paymentVerification,
   getVerifiedTransactions,
+  getPayment,
 } = Payment;
 
 const paymentRouter = express.Router();
 
 paymentRouter.patch('/verify/:transaction_id', isUserLoggedIn, paymentVerification);
 paymentRouter.get('/transactions', isUserLoggedIn, isUserAdmin, getVerifiedTransactions);
+paymentRouter.get('/payment', isUserLoggedIn, getPayment);
 
 export default paymentRouter;
